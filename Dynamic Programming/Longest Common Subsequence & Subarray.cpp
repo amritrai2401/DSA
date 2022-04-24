5392dbf750db898e352dae9d1da75294890924de
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-24 12:29:13 +0530

    Update Longest Common Subsequence & Subarray.cpp

--- Dynamic Programming/Longest Common Subsequence & Subarray.cpp
+++ Dynamic Programming/Longest Common Subsequence & Subarray.cpp
@@ -33,3 +33,33 @@ reverse(s.begin(),s.end());
 // LRS:Longest repeting subsequence of an arr : aabbdd =3 abd coming 2times: x=arr,y=arr; lcs(x,y) with restriction if(X[i-1]==Y[j-1] && i!=j).....
 // Check if x is a subsequence of y : if(lcs(x,y)==x.size()) cout<<"yes"; 
 
+// REGULAR STRING MATCHING
+// class Solution {
+//    public boolean isMatch(String s, String p) {
+
+//     boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+//     dp[0][0] = true;
+//     for (int i = 0; i < p.length(); i++) {
+//         if (p.charAt(i) == '*' && dp[0][i-1]) {
+//             dp[0][i+1] = true;
+//         }
+//     }
+//     for (int i = 1 ; i <= s.length(); i++) {
+//         for (int j = 1; j <= p.length(); j++) {
+//             if (p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.') {
+//                 dp[i][j] = dp[i-1][j-1];
+//             }
+//             else if (p.charAt(j-1) == '*') {
+//                 dp[i][j]=dp[i][j-2];
+//                 if (p.charAt(j-2) == s.charAt(i-1) || p.charAt(j-2) == '.')
+//                     dp[i][j] = (dp[i][j-1] || dp[i-1][j] || dp[i][j-2]);
+//             }
+            
+//         }
+//     }
+//     return dp[s.length()][p.length()];
+// }
+// }
+
+
+
