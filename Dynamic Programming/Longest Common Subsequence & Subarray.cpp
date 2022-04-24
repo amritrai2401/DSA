56c2eb149c4afce58b9624d043f2b6f47cb477c6
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-24 10:08:09 +0530

    Update Longest Common Subsequence & Subarray.cpp

--- Dynamic Programming/Longest Common Subsequence & Subarray.cpp
+++ Dynamic Programming/Longest Common Subsequence & Subarray.cpp
@@ -4,7 +4,7 @@ int lcs(string X, string Y) {
         
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(X[i-1] == Y[j-1]) dp[i][j] = dp[i-1][j-1] + 1;  //For substring : {....+1; ans=max(ans,dp[i][j]);}
+                if(X[i-1] == Y[j-1]) dp[i][j] = dp[i-1][j-1] + 1;                      //For substring : {....+1; ans=max(ans,dp[i][j]);}
                 else dp[i][j] = max(dp[i][j-1]/*insert*/, dp[i-1][j]/*remove*/);       //For substring : dp[i][j]=0;
             }
         }
