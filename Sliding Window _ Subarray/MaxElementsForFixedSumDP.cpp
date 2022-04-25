1cc64f872649cafe97b9f87685c7735461026e36
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-25 18:32:41 +0530

    Update MaxElementsForFixedSumDP.cpp

--- Sliding Window _ Subarray/MaxElementsForFixedSumDP.cpp
+++ Sliding Window _ Subarray/MaxElementsForFixedSumDP.cpp
@@ -1,15 +1,40 @@
 Tc=o(n)
-//Sliding window is only  for positive array
-int subarraySum(vector<int>& nums, int k) {
-        map<int, int> seen = {{0, 1}};
-        int count = 0, sum = 0;
-        for (auto n: nums) {
-            sum += n;
-            count += seen[sum - k];
-            seen[sum]++;
+//Sliding window is only  for positive array   this works for both pos and neg  longest subarray with 0 sum
+
+    int maxLen(vector<int>&arr, int n)
+    {   int k=0;
+            unordered_map<int, int> um;
+    int sum = 0, maxLen = 0;
+ 
+    // traverse the given array
+    for (int i = 0; i < n; i++) {
+ 
+        // accumulate sum
+        sum += arr[i];
+ 
+        // when subarray starts from index '0'
+        if (sum == k)
+            maxLen = i + 1;
+ 
+        // make an entry for 'sum' if it is
+        // not present in 'um'
+        if (um.find(sum) == um.end())
+            um[sum] = i;
+ 
+        // check if 'sum-k' is present in 'um'
+        // or not
+        if (um.find(sum - k) != um.end()) {
+ 
+            // update maxLength
+            if (maxLen < (i - um[sum - k]))
+                maxLen = i - um[sum - k];
         }
-        return count;
-}
+    }
+ 
+    // required maximum length
+    return maxLen;
+    }
+
 
 //Longest substring without repeting characters
 int lengthOfLongestSubstring(string s) {
