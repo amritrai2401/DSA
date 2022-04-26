270bb903363da1aa3afa65d0550d4396bd3d3572
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-27 00:22:16 +0530

    Update MaxElementsForFixedSumDP.cpp

--- Sliding Window _ Subarray/MaxElementsForFixedSumDP.cpp
+++ Sliding Window _ Subarray/MaxElementsForFixedSumDP.cpp
@@ -1,39 +1,20 @@
 Tc=o(n)
 
 
-    int maxLen(vector<int>&arr, int k)//k=sum
-    {   
-            int n=arr.size();
-            unordered_map<int, int> um;
+int maxLen(vector<int>&arr, int k)//k=sum{   
+    int n=arr.size();
+    map<int, int> um;
     int sum = 0, maxLen = 0;
- 
-    // traverse the given array
     for (int i = 0; i < n; i++) {
- 
-        // accumulate sum
         sum += arr[i];
- 
-        // when subarray starts from index '0'
         if (sum == k)
             maxLen = i + 1;
- 
-        // make an entry for 'sum' if it is
-        // not present in 'um'
         if (um.find(sum) == um.end())
             um[sum] = i;
- 
-        // check if 'sum-k' is present in 'um'
-        // or not
         if (um.find(sum - k) != um.end()) {
- 
-            // update maxLength
             if (maxLen < (i - um[sum - k]))
                 maxLen = i - um[sum - k];
         }
     }
- 
-    // required maximum length
     return maxLen;
-    }
-
-//Works for both pos and neg array
+}
