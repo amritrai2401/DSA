105eb50679adce26dcbae749d3bd5ba90f9744a1
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-27 12:27:49 +0530

    Update Largest subarray with Unique characters.cpp

--- Sliding Window _ Subarray/Largest subarray with Unique characters.cpp
+++ Sliding Window _ Subarray/Largest subarray with Unique characters.cpp
@@ -1,25 +1,13 @@
-//Longest subarray without repeting characters
-int largest_subarray(int a[], int n)
-{
-    // Stores index of array elements
-    unordered_map<int, int> index;
+int largest_subarray(int a[], int n){
+    map<int, int> index;
     int ans = 0;
     for (int i = 0, j = 0; i < n; i++) {
-  
-        // Update j based on previous
-        // occurrence of a[i]
         j = max(index[a[i]], j);
-  
-        // Update ans to store maximum
-        // length of subarray
+        
         ans = max(ans, i - j + 1);
-  
-        // Store the index of current
-        // occurrence of a[i]
+
         index[a[i]] = i + 1;
     }
-  
-    // Return final ans
     return ans;
 }
 
