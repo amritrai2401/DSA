34b7398c0e8b42c1f35966afc7cf10da1d47c193
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-27 12:27:46 +0530

    Update and rename MaxElementsForFixedSumDP.cpp to Largest subarray for FixedSumDP.cpp

--- Sliding Window _ Subarray/Largest subarray for FixedSumDP.cpp
+++ Sliding Window _ Subarray/Largest subarray for FixedSumDP.cpp
@@ -1,19 +1,16 @@
-Tc=o(n)
-
-
+Tc=o(nlogn)
 int maxLen(vector<int>&arr, int k)//k=sum{   
     int n=arr.size();
     map<int, int> um;
     int sum = 0, maxLen = 0;
     for (int i = 0; i < n; i++) {
         sum += arr[i];
-        if (sum == k)
-            maxLen = i + 1;
+        if (sum == k) maxLen = i + 1;
         if (um.find(sum) == um.end())
             um[sum] = i;
         if (um.find(sum - k) != um.end()) {
             if (maxLen < (i - um[sum - k]))
-                maxLen = i - um[sum - k];
+                maxLen = i - um[sum - k];       //From um[sum-k] to i milgaya 
         }
     }
     return maxLen;
