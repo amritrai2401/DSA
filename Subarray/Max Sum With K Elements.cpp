63833194811063a98fcf2a967fedba934761342d
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-28 18:37:29 +0530

    Update Max Sum With K Elements.cpp

--- Subarray/Max Sum With K Elements.cpp
+++ Subarray/Max Sum With K Elements.cpp
@@ -9,4 +9,23 @@ int maxSum(int arr[], int n, int k){
     return res;
 }
 
-//First negative in every window        Anagram 
+
+Anagram :
+vector<int> findAnagrams(string s, string p) {             
+        vector<int> freq_p(26,0);
+        vector<int> window(26,0);
+        vector<int> ans;
+    
+        for(int i=0;i<p.size();i++){
+            freq_p[p[i]-'a']++;
+            window[s[i]-'a']++;
+        }
+        if(freq_p == window) ans.push_back(0);
+        
+        for(int i=p.size();i<s.size();i++){
+            window[s[i-p.size()] - 'a']--;
+            window[s[i] - 'a']++;
+            if(freq_p == window) ans.push_back(i-p.size()+1);
+        }
+        return ans;  //Returns starting position of all anagrams. ans.size()= total no of anagrams.
+}
