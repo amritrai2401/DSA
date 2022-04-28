ef9c2a46af20084fbb9911838c50b1f8f2ba8f7a
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-28 10:30:37 +0530

    Update Largest subarray for k Sum(DP).cpp

--- Sliding Window _ Subarray/Largest subarray for k Sum(DP).cpp
+++ Sliding Window _ Subarray/Largest subarray for k Sum(DP).cpp
@@ -1,17 +1,18 @@
 Tc=o(nlogn)
 int maxLen(vector<int>&arr, int k)//k=sum{   
     int n=arr.size();
-    map<int, int> um;
+    map<int, int>m;
     int sum = 0, maxLen = 0;
     for (int i = 0; i < n; i++) {
         sum += arr[i];
-        if (sum == k) maxLen = i + 1;
-        if (um.find(sum) == um.end())
-            um[sum] = i;
-        if (um.find(sum - k) != um.end()) {
-            if (maxLen < (i - um[sum - k]))
-                maxLen = i - um[sum - k];       //From um[sum-k] to i milgaya 
+        if (sum == k) maxLen = i + 1;              //For total subarrays with sum k : if(..)tot++;   //For Index of largest subarray : if(..) 0 to i;
+        if (m.find(sum) == m.end())
+            m[sum] = i;
+        if (m.find(sum - k) != m.end()) {
+                                                   //For total subarrays with sum k :total+=um[sum-k]; 
+            if (maxLen < (i - m[sum - k]))
+                maxLen = i - m[sum - k];           //For Index of largest subarray : if(..) m[sum-k] to i;
         }
     }
-    return maxLen;
+    return maxLen;                                 //For total subarrays with sum k : return tot;
 }
