c475530e399124026bbfacf42695e9bac5fd7291
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-30 11:23:07 +0530

    Update Buy and Sell k  Transactions with fee and cooldown.cpp

--- Dynamic Programming/Buy and Sell k  Transactions with fee and cooldown.cpp
+++ Dynamic Programming/Buy and Sell k  Transactions with fee and cooldown.cpp
@@ -5,11 +5,11 @@ int maxProfit(int transactions, vector<int>& prices, int fee) {
         for(int i=0; i<transactions;i++) buy[i][0] = -prices[0];      
         
         for(int i=1; i<prices.size(); i++) {                           
-            int prevInvestment = 0;                                     // For 1 transaction : previnve=always 0, so ignore this line
+            int prevInvestment = 0;                                     // For 1 transaction : no meaning wheather u write or not  
             for(int j=0; j<transactions; j++) {                         // For 1 transaction : for(i=0 to 0) = 1 times so you can ignore this loop
                 buy[j][i] = max(buy[j][i-1], prevInvestment-prices[i]);
                 sell[j][i] = max(sell[j][i-1], buy[j][i-1] + prices[i]-fee);
-                prevInvestment = i-cooldown>0?sell[j][i-cooldown]:0;    // For 1 transaction : no meaning  so ignore                     
+                prevInvestment = i-cooldown>0?sell[j][i-cooldown]:0;    // For 1 transaction : no meaning wheather u write or not                    
             }
         }
         int ans = 0;
