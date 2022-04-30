c76fcaf98754d16c4cdc1bf34c264096e2b66d34
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-30 12:30:35 +0530

    Update Buy and Sell ∞ Transactions with fee and cooldown.cpp

--- Dynamic Programming/Buy and Sell ∞ Transactions with fee and cooldown.cpp
+++ Dynamic Programming/Buy and Sell ∞ Transactions with fee and cooldown.cpp
@@ -1,14 +1,13 @@
 int maxProfit(vector<int>& prices, int cooldown,int fee) {
         int n = prices.size();
         if (n <= 1) return 0;
-        vector<int> buy(n), sell(n);  // OR vi buy(n) ,sell(n+cooldown);
+        vector<int> buy(n), sell(n);
         buy[0] = -prices[0];
         for (int i = 1; i < n; i++) {
-            buy[i]=max(buy[i-1],i-cooldown>0? sell[i-cooldown-1]-prices[i] : 0-prices[i]); // OR buy[i] = max(buy[i-1], sell[i-1] - prices[i]);   
-            sell[i]=max(sell[i-1],buy[i-1]+prices[i]-fee);                                // OR sell[i+cooldown] = max(sell[i+cooldown-1], buy[i-1] + prices[i]-fee);
+            buy[i]=max(buy[i-1],sell[i-1]-prices[i]);
+            sell[i]=max(sell[i-1],buy[i-1]+prices[i]-fee);                             
         }
-        return sell[n-1];   // OR return sell[n+cooldown-1];
+        return sell[n-1];   
 }
-
+// If cooldown asked with ∞ transactions then solution will be o(n^2) with assuming transactions=n-1
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/discuss/1167126/C%2B%2B-Solution-to-all-the-Stock-Problems-using-Dynamic-Programming
-        
