fe83c5802ce443c411dca901aec74119861452fb
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-04-30 12:02:38 +0530

    Update Buy and Sell ∞ Transactions with fee and cooldown.cpp

--- Dynamic Programming/Buy and Sell ∞ Transactions with fee and cooldown.cpp
+++ Dynamic Programming/Buy and Sell ∞ Transactions with fee and cooldown.cpp
@@ -1,7 +1,7 @@
 int maxProfit(vector<int>& prices, int cooldown,int fee) {
         int n = prices.size();
         if (n <= 1) return 0;
-        vector<int> buy(n), sell(n + cooldown);  // OR vi buy(n) ,sell(n);
+        vector<int> buy(n), sell(n);  // OR vi buy(n) ,sell(n+cooldown);
         buy[0] = -prices[0];
         for (int i = 1; i < n; i++) {
             buy[i]=max(buy[i-1],i-cooldown>0? sell[i-cooldown-1]-prices[i] : 0-prices[i]); // OR buy[i] = max(buy[i-1], sell[i-1] - prices[i]);   
