73b32cf8fa640340319a3fd3f9f317cbe62d9bd6
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-05-01 12:24:31 +0530

    Update and rename Largest subarray for k Sum(DP).cpp to Total or Largest subarray for k Sum(DP).cpp

--- Hashing and Sliding window/Total or Largest subarray for k Sum(DP).cpp
+++ Hashing and Sliding window/Total or Largest subarray for k Sum(DP).cpp
@@ -16,3 +16,18 @@ int maxLen(vector<int>&arr, int k)//k=sum{
     }
     return maxLen;                                 //For total subarrays with sum k : return tot;
 }
+
+
+Total:
+    int subarraySum(vector<int>& nums, int k) {
+        int cum=0; // cumulated sum
+        map<int,int> rec; // prefix sum recorder
+        int cnt = 0; // number of found subarray
+        rec[0]++; // to take into account those subarrays that begin with index 0
+        for(int i=0;i<nums.size();i++){
+            cum += nums[i];
+            cnt += rec[cum-k];
+            rec[cum]++;
+        }
+        return cnt;
+    }
