1c600cece0a0a0db828eae16222cf9d04d3c15de
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-05-01 22:35:55 +0530

    Update Matrix Path.cpp

--- Dynamic Programming/Matrix Path.cpp
+++ Dynamic Programming/Matrix Path.cpp
@@ -1,3 +1,4 @@
+//PATH QUE CAN NEVER ASK PERMUTATION. ex; 1->2->3 you cant permute it like first go 3 then 1 then 2 (3->1->1)wrong
 int solve(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size(); 
@@ -9,59 +10,22 @@ int solve(vector<vector<int>>& grid) {
                 dp[i][j]  = min(dp[i - 1][j],dp[i][j - 1]) + grid[i][j];   // For no of ways to reach : dp[i-1][j]+dp[i][j-1]
         return dp[m - 1][n - 1];
 }
-//Path problems are combinational not permutational
 
 
-
-right-up (d1), 1 cell right (d2) or 1 cell right-down(d3).  but not circular
-      int[][] dp = new int[arr.length][arr[0].length];
+// Que:You are standing in top-left cell and are required to move to bottom-right cell and you are allowed to move 1 cell right (h move) or 1 cell down (v move) in 1 motion.
+// Sol:
+//       int[][] dp = new int[arr.length][arr[0].length];
       
-      for(int j = arr[0].length - 1; j >= 0; j--){
-          for(int i = 0; i < arr.length; i++){
-              if(j == arr[0].length - 1){
-                  dp[i][j] = arr[i][j];
-              } else if(i == 0){
-                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
-              } else if(i == arr.length - 1){
-                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
-              } else {
-                  dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i - 1][j + 1], dp[i + 1][j + 1]));
-              }
-          }
-      }
-      
-      int max = Integer.MIN_VALUE;
-      for(int i = 0; i < dp.length; i++){
-          if(dp[i][0] > max){
-              max = dp[i][0];
-          }
-      }
-
-
-
-
-
-
-
-
-
-
-
-4. You are standing in top-left cell and are required to move to bottom-right cell.
-5. You are allowed to move 1 cell right (h move) or 1 cell down (v move) in 1 motion.
-      
-      int[][] dp = new int[arr.length][arr[0].length];
-      
-      for(int i = dp.length - 1; i >= 0; i--){
-          for(int j = dp[0].length - 1; j >= 0; j--){
-              if(i == dp.length - 1 && j == dp[0].length - 1){
-                  dp[i][j] = arr[i][j];
-              } else if(i == dp.length - 1){
-                  dp[i][j] = arr[i][j] + dp[i][j + 1];
-              } else if(j == dp[0].length - 1){
-                  dp[i][j] = arr[i][j] + dp[i + 1][j];
-              } else {
-                  dp[i][j] = arr[i][j] + Math.min(dp[i][j + 1], dp[i + 1][j]);
-              }
-          }
-      }
+//       for(int i = dp.length - 1; i >= 0; i--){
+//           for(int j = dp[0].length - 1; j >= 0; j--){
+//               if(i == dp.length - 1 && j == dp[0].length - 1){
+//                   dp[i][j] = arr[i][j];
+//               } else if(i == dp.length - 1){
+//                   dp[i][j] = arr[i][j] + dp[i][j + 1];
+//               } else if(j == dp[0].length - 1){
+//                   dp[i][j] = arr[i][j] + dp[i + 1][j];
+//               } else {
+//                   dp[i][j] = arr[i][j] + Math.min(dp[i][j + 1], dp[i + 1][j]);
+//               }
+//           }
+//       }
