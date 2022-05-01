0196c231a57fdc3ba0c0222d4112b368c029253a
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-05-01 22:25:31 +0530

    Update All Maze Paths with jumps.cpp

--- Recursion/All Maze Paths with jumps.cpp
+++ Recursion/All Maze Paths with jumps.cpp
@@ -65,65 +65,25 @@ int knightTour(int x, int y, int move, int sol[N][N], int xMove[N], int yMove[N]
  [0,9,0]]
  ans=9 -> 8 -> 7.
 static vector<int> directions={0,1,0,-1,0};
-class Solution {
-public:
-    int dfs(vector<vector<int>> &grid,int i,int j)
-    {
+
+int dfs(vector<vector<int>> &grid,int i,int j){
         int x,y,temp=grid[i][j],result=0;
         grid[i][j]=0;
-        for(int d=0;d<4;d++)                    //Try all 4 possible directions
-        {
+        for(int d=0;d<4;d++){                   //Try all 4 possible directions
             x=i+directions[d],y=j+directions[d+1];
             if(x>=0&&y>=0&&x<grid.size()&&y<grid[x].size()&&grid[x][y]!=0)
                 result=max(result,grid[x][y]+dfs(grid,x,y));  //Set grid[i][j] to 0 to mark as visited before next dfs and reset the values after the dfs ends
         }
         grid[i][j]=temp;
         return result;
-    }
-    int getMaximumGold(vector<vector<int>>& grid) 
-    {
+}
+    
+int getMaximumGold(vector<vector<int>>& grid) {
         int result=0;
         for(int i=0;i<grid.size();i++)
             for(int j=0;j<grid[i].size();j++)
                 if(grid[i][j]>0)
                     result=max(grid[i][j]+dfs(grid,i,j),result);   //Set grid[i][j] to 0 to mark this cell as visited and reset after the dfs ends.
         return result;
-    }
-};
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    double helperFunction(int N, int K, int r, int c, vector<vector<vector<double>>> & dp){
-        if(r < 0 || c < 0 || r >= N || c >= N) return 0;
-        if(K == 0) return 1;
-        if(dp[r][c][K] != -1) return dp[r][c][K];
-        double sum = helperFunction(N, K - 1, r - 2, c - 1, dp) +
-                     helperFunction(N, K - 1, r - 1, c - 2, dp) +
-                     helperFunction(N, K - 1, r + 1, c - 2, dp) +
-                     helperFunction(N, K - 1, r + 2, c - 1, dp) +
-                     helperFunction(N, K - 1, r - 2, c + 1, dp) +
-                     helperFunction(N, K - 1, r - 1, c + 2, dp) +
-                     helperFunction(N, K - 1, r + 1, c + 2, dp) +
-                     helperFunction(N, K - 1, r + 2, c + 1, dp);
-        sum = sum / 8;
-        dp[r][c][K] = sum;
-        return dp[r][c][K];
-    
-    }
-    double knightProbability(int N, int K, int r, int c) {
-        vector<vector<vector<double>>> dp(N + 1, vector<vector<double>> (N + 1, vector<double>(K + 1, -1)));
-        return helperFunction(N, K, r, c, dp);
-    }
