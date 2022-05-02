aa4666bbc5b2f576cf3ad101d37a1b81c05271ad
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-05-02 19:23:37 +0530

    Update and rename Matrix traversal or Matrix paths from 1 cell.cpp to Matrix traversal from a cell.cpp

--- Recursion/Matrix traversal from a cell.cpp
+++ Recursion/Matrix traversal from a cell.cpp
@@ -1,4 +1,59 @@
-//All Matrix Paths with any number of jumps allowed
+//Knight Tour ::
+
+int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+bool isValid(int x, int y, int sol[N][N]) {
+   return ( x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == -1);
+}
+
+int knightTour(int x, int y, int move, int sol[N][N], int xMove[N], int yMove[N]) {
+   if (move == N*N) return true;
+
+   for (int k = 0; k < 8; k++) {  //NO OF MOVES knight can move NOT NO of rows or columns 
+      int xNext = x + xMove[k], yNext = y + yMove[k];
+      if (isValid(xNext, yNext, sol)) {
+         sol[xNext][yNext] = move;
+         if (knightTour(xNext, yNext, move+1, sol, xMove, yMove) == true) return true;
+         else sol[xNext][yNext] = -1;
+      }
+   }
+   return false;
+}
+
+
+
+// Maximum Gold 
+
+static vector<int> directions={0,1,0,-1,0};
+
+int dfs(vector<vector<int>> &grid,int i,int j){
+        int x,y,temp=grid[i][j],result=0;
+        grid[i][j]=0;
+        for(int d=0;d<4;d++){                   //Try all 4 possible directions
+            x=i+directions[d],y=j+directions[d+1];
+            if(x>=0&&y>=0&&x<grid.size()&&y<grid[x].size()&&grid[x][y]!=0)
+                result=max(result,grid[x][y]+dfs(grid,x,y));  //Set grid[i][j] to 0 to mark as visited before next dfs and reset the values after the dfs ends
+        }
+        grid[i][j]=temp; // If you are running multiple dfs i.e, dfs from multiple cells then to make visited or grid in its original form to reused in another one 
+        return result;
+}
+    
+int getMaximumGold(vector<vector<int>>& grid) {
+        int result=0;
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[i].size();j++)
+                if(grid[i][j]>0)
+                    result=max(grid[i][j]+dfs(grid,i,j),result);   //Set grid[i][j] to 0 to mark this cell as visited and reset after the dfs ends.
+        return result;
+}
+
+
+
+
+
+
+//All Matrix Paths with any number of jumps allowed /Nados
 
 vector<string> get_maze_paths(int sr, int sc, int dr, int dc) {
     if(sr == dr && sc == dc){
@@ -32,62 +87,5 @@ vector<string> get_maze_paths(int sr, int sc, int dr, int dc) {
         }
         }
     return ans;
-}
-
-
-
-
-
-
-//Knight Tour ::
-
-int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-
-bool isValid(int x, int y, int sol[N][N]) {
-   return ( x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == -1);
-}
-
-int knightTour(int x, int y, int move, int sol[N][N], int xMove[N], int yMove[N]) {
-   int xNext, yNext;
-   if (move == N*N) return true;
-
-   for (int k = 0; k < 8; k++) {  //NO OF MOVES knight can move NOT NO of rows or columns 
-      xNext = x + xMove[k];
-      yNext = y + yMove[k];
-      if (isValid(xNext, yNext, sol)) {
-         sol[xNext][yNext] = move;
-         if (knightTour(xNext, yNext, move+1, sol, xMove, yMove) == true) return true;
-         else sol[xNext][yNext] = -1;
-      }
-   }
-   return false;
-}
-
-
-
-// Maximum Gold 
-
-static vector<int> directions={0,1,0,-1,0};
-
-int dfs(vector<vector<int>> &grid,int i,int j){
-        int x,y,temp=grid[i][j],result=0;
-        grid[i][j]=0;
-        for(int d=0;d<4;d++){                   //Try all 4 possible directions
-            x=i+directions[d],y=j+directions[d+1];
-            if(x>=0&&y>=0&&x<grid.size()&&y<grid[x].size()&&grid[x][y]!=0)
-                result=max(result,grid[x][y]+dfs(grid,x,y));  //Set grid[i][j] to 0 to mark as visited before next dfs and reset the values after the dfs ends
-        }
-        grid[i][j]=temp;
-        return result;
-}
-    
-int getMaximumGold(vector<vector<int>>& grid) {
-        int result=0;
-        for(int i=0;i<grid.size();i++)
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j]>0)
-                    result=max(grid[i][j]+dfs(grid,i,j),result);   //Set grid[i][j] to 0 to mark this cell as visited and reset after the dfs ends.
-        return result;
 }
 
