5343278c9166d4392dfdcb0a7e7686819809988f
Amrit Rai <86003701+amritrai5757@users.noreply.github.com>
2022-05-02 19:13:18 +0530

    Update Subset Combination Unbounded & 01(Subsequences).cpp

--- Recursion/Subset Combination Unbounded & 01(Subsequences).cpp
+++ Recursion/Subset Combination Unbounded & 01(Subsequences).cpp
@@ -5,7 +5,7 @@ void helper(vector<vector<int>> &res,vector<int>& candidates,vector<int> &combin
         if(target<0)return;
         for(int i =c; i < candidates.size() ; i++){
             if(i>c && candidates[i] == candidates[i-1])continue;  //If bounded prob sort the arr first.For unique combination in bounded problem like 121 allowed but again 211 not allowed
-            combination.emplace_back(candidates[i]);
+            combination.push_back(candidates[i]);
             helper(res,candidates,combination,target - candidates[i],i+1); //For Unbounded: ....,target-candidates[i],i);
             combination.pop_back();
         }
